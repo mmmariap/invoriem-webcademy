c6ff67dd2bcf65b6dc50077b6aa2b2f6fac7e314
Maria P. <mariapyanykh@gmail.com>
2025-09-24 01:57:05 +0300

    section about (video)

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@ $(document).ready(function(){
     loop: true
   });
 });
+
+/* Video */
+const videoBtn = document.querySelector('#video-btn');
+const videoPicture = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+videoWrapper.addEventListener('click', function () {
+
+  if (video.paused) {
+    videoPicture.classList.add('none');
+    videoWrapper.classList.remove('video-overlay');
+    videoBtn.classList.add('none');
+    video.play();
+  } else {
+    videoWrapper.classList.add('video-overlay');
+    videoBtn.classList.remove('none');
+    video.pause();
+  }
+})
